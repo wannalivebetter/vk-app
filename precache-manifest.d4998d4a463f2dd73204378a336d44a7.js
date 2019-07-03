d9cef8e17a3aaf0b267bcaff904d91f1b6c61261
Алексей Юрков <lervj@Aleksejs-MacBook-Pro.local>
2019-07-03 21:38:32 +0300

    Updates

---
+++ precache-manifest.d4998d4a463f2dd73204378a336d44a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f30eac9572f766c1a3d4",
+    "url": "/vk-app/static/css/main.9f283e45.chunk.css"
+  },
+  {
+    "revision": "f30eac9572f766c1a3d4",
+    "url": "/vk-app/static/js/main.bfd417e4.chunk.js"
+  },
+  {
+    "revision": "3ea2b26cbdeb852cdfc0",
+    "url": "/vk-app/static/js/runtime~main.06aa4575.js"
+  },
+  {
+    "revision": "6140af22cc524f519f2a",
+    "url": "/vk-app/static/css/2.731afa27.chunk.css"
+  },
+  {
+    "revision": "6140af22cc524f519f2a",
+    "url": "/vk-app/static/js/2.ad4c9ac3.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-app/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "0e860e5f05112ff2fb5923e63e113f51",
+    "url": "/vk-app/index.html"
+  }
+];
